9150f1753a722a3b8cd62518f70c1d43ced8c7e5
ZenithalHourlyRate <i@zenithal.me>
2022-04-14 16:11:33 +0800

    opentuna: update options

--- tunasync/config.py
+++ tunasync/config.py
@@ -3,7 +3,7 @@ sites = {
     "site": "https://mirrorz.org/static/json/site/opentuna.json",
     "tunasync": "https://opentuna.cn/jobs",
     "info": "https://opentuna.cn/isoinfo.json",
-    "options": "https://mirrors.tuna.tsinghua.edu.cn/static/js/options.json",
+    "options": "https://opentuna.cn/static/js/options.json",
     "cname": "https://mirrorz.org/static/json/cname.json",
   },
 }
